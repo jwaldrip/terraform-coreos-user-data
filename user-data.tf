4bc0a80979fb30175bc552f87796880ac64e9e42
jwaldrip <jason@waldrip.net>
2016-09-08 18:35:52 -0600

    fix specs

--- user-data.tf
+++ user-data.tf
@@ -2,7 +2,12 @@ resource "template_file" "user-data" {
   template = "${file("${path.module}/templates/user-data.yml")}"
 
   provisioner "local-exec" {
-    command = "curl -s 'https://validate.core-os.net/validate' -X PUT -H 'Content-Type: text/plain' -H 'Accept: application/json' --data-binary @- <<'__USERDATA__' | tee /dev/stderr | grep --invert-match -E '(error|warning)' > /dev/null\n${template_file.user-data.rendered}\n__USERDATA__"
+    command = <<-EOF
+      set -e
+      curl -sSLfk "https://validate.core-os.net/validate" -X PUT -H "Content-Type: text/plain" -H "Accept: application/json" --data-binary @- <<'__USERDATA__' | tee /dev/stderr | if grep -E "error|warning" ; then false ; else true ; fi
+${template_file.user-data.rendered}
+__USERDATA__
+    EOF
   }
 
   vars {
